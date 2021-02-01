97f371892d98ada09c706d28dde8bfa8100cdec2
Namrata Kulkarni <NKulkarni0@walmartlabs.com>
2021-02-01 13:55:50 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState([]);
+  const [inputVal, setInputVal] = useState("");
+  //arr[i] += arr[i+1];
+  const addToList = (text) => {
+    setValue([...value, text]);
+    return value;
+  }
+
+//remove item from the end of the list
+  console.log("value" + value);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={inputVal} onChange={(e) => {setInputVal(e.currentTarget.value)}} />
+      <p> {inputVal} </p>
+      <ul>
+        {value.map((v,index) =>
+          <div>
+            <li>{v}</li>
+            <button onClick={(index) => setValue(value.slice(0, -1))}>Remove</button>
+          </div>
+         )}
+        </ul>
+        <button onClick={() => addToList(inputVal)}>Submit</button>
     </div>
   );
 }
 
 export default App;
+
+
+//input field with submit button
+//add to input it will display as a list
